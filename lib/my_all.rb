7b09726e126ac62de38f87988cd53c399cf8dc55
Will Carter <will.carter@fergusllc.com>
2019-12-23 17:30:36 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,8 +3,8 @@ require 'pry'
 def my_all?(collection)
   i = 0
   block_return_values = []
-  binding.pry
   while i < collection.length
+    binding.pry
     block_return_values << yield(collection[i])
     i =+ 1
   end
